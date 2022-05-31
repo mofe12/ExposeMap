af41c59e9ebcc52cb0ecceeb6c0f662f61d2ca18
mofe123 <mofe12>
2022-05-31 15:12:20 -0400

    the ML full push

--- Expose2.0/View/MLContentView.swift
+++ Expose2.0/View/MLContentView.swift
@@ -12,7 +12,7 @@ struct MLContentView: View {
     @EnvironmentObject var viewModel : MapUIViewModel
     var body: some View {
         VStack {
-            Image(uiImage: viewModel.photosToBeScanned[0])
+            Image(viewModel.photosToBeScanned[0])
                 .resizable()
                 .frame(width: 200, height: 200)
             
@@ -21,7 +21,7 @@ struct MLContentView: View {
                 // Add more code here
                 viewModel.MLPhotoResults = []
                 for photo in viewModel.photosToBeScanned {
-                    viewModel.classifyImage(currentImageName: photo)
+                    classifyImage(currentImageName: photo)
                 }
                 
             }
@@ -39,26 +39,26 @@ struct MLContentView: View {
         }
     }
     
-//    private func classifyImage(currentImageName: String) {
-//        //let currentImageName = photos[currentIndex]
-//
-//        guard let image = UIImage(named: currentImageName),
-//              let resizedImage = image.resizeImageTo(size:CGSize(width: 224, height: 224)),
-//              let buffer = resizedImage.convertToBuffer() else {
-//              return
-//        }
-//
-//        let output = try? viewModel.model.prediction(image: buffer)
-//
-//        if let output = output {
-//            let results = output.classLabelProbs.sorted { $0.1 > $1.1 }
-//            //let result = results.map { (key, value) in
-//            //    return "\(key) = \(String(format: "%.2f", value * 100))%"
-//            //}.joined(separator: "\n")
-//            let result = results.first
-//            viewModel.MLPhotoResults.append(result?.key ?? "No result")
-//        }
-//    }
+    private func classifyImage(currentImageName: String) {
+        //let currentImageName = photos[currentIndex]
+        
+        guard let image = UIImage(named: currentImageName),
+              let resizedImage = image.resizeImageTo(size:CGSize(width: 224, height: 224)),
+              let buffer = resizedImage.convertToBuffer() else {
+              return
+        }
+        
+        let output = try? viewModel.model.prediction(image: buffer)
+        
+        if let output = output {
+            let results = output.classLabelProbs.sorted { $0.1 > $1.1 }
+            //let result = results.map { (key, value) in
+            //    return "\(key) = \(String(format: "%.2f", value * 100))%"
+            //}.joined(separator: "\n")
+            let result = results.first
+            viewModel.MLPhotoResults.append(result?.key ?? "No result")
+        }
+    }
     
 }
 
--- Expose2.0/ViewModel/MapViewModel.swift
+++ Expose2.0/ViewModel/MapViewModel.swift
@@ -7,8 +7,6 @@
 
 import SwiftUI
 import MapKit
-import UIKit
-import PhotosUI
 import CoreLocation
 
 // All Map Data Goes here....
@@ -45,15 +43,10 @@ final class MapUIViewModel: NSObject, ObservableObject, CLLocationManagerDelegat
     @Published var showInterestListView = false
     
     // Photos to be scanned by the ML
-    
-    // Showing image picker
-    @Published var isShowingImagePicker = false
-    
-    @Published var photosToBeScanned: [UIImage] = []
+    @Published var photosToBeScanned: [String] = ["lemon","strawberry", "test","basketball","soccer","climb"]
     
     // Current Interest
     @Published var currentInterest: String = "SELECT INTEREST"
-
     
     // ML results
     @Published var MLPhotoResults : [String] = ["chess", "guitar", "car"]
@@ -68,21 +61,6 @@ final class MapUIViewModel: NSObject, ObservableObject, CLLocationManagerDelegat
                  , county: "test"
                  , country: "US"
                  , zipCode: "94133")
-    
-    
-    // Handle result
-    func handleResults(_ results: [PHPickerResult]) {
-      for result in results {
-        result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] imageObject, error in
-          guard let image = imageObject as? UIImage else { return }
-            guard let data = image.jpegData(compressionQuality: 1), let compressedImage = UIImage(data: data) else{return}
-          DispatchQueue.main.async { [weak self] in
-            self?.photosToBeScanned.append(compressedImage)
-          }
-        }
-      }
-    }
-
     
     
     // Search Places and puts result in placeArray
@@ -148,12 +126,11 @@ final class MapUIViewModel: NSObject, ObservableObject, CLLocationManagerDelegat
     }
     
     // Classifying image for ML
-    func classifyImage(currentImageName: UIImage) {
+    func classifyImage(currentImageName: String) {
         //let currentImageName = photos[currentIndex]
         
-       // guard let image = UIImage(named: currentImageName),
-        let image = currentImageName
-            guard let resizedImage = image.resizeImageTo(size:CGSize(width: 224, height: 224)),
+        guard let image = UIImage(named: currentImageName),
+              let resizedImage = image.resizeImageTo(size:CGSize(width: 224, height: 224)),
               let buffer = resizedImage.convertToBuffer() else {
             return
         }
